7a5decdc4722134c381b47ebd528357b4211e3c7
DabblerGISer <badcalf97@163.com>
2021-10-06 16:59:42 +0200

    first test

---
+++ code/test.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+
+updater = Updater(token='2068690659:AAEfQ150bLudfpMe5QbKGJS8liTip3gU4U0')
+
+dispatcher= updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
